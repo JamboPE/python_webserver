fbf67508deab9baee0ea0dbdb695dcbdd521f0e2
JamboPE <github@jdpe.co.uk>
2024-01-10 01:34:34 +0000

    Added CSS

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', background_color="", text_color="")
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def form():
@@ -13,17 +13,6 @@ def form():
         print("form entry")
         alice_bits = request.form['alice_string']
         bob_bits = request.form['bob_string']
-        try:
-            dark_mode
-        except:
-            dark_mode = False
-        if dark_mode == True:
-            return render_template('form return.html',alice_bits=alice_bits,bob_bits=bob_bits,corrected_string="corrected_string",no_parity="no_parity",no_errors="no_errors",shannon_limit="shannon_limit",no_itterations="no_itterations", background_color="rgb(80, 80, 80)", text_color="white")
-        else:
-            return render_template('form return.html',alice_bits=alice_bits,bob_bits=bob_bits,corrected_string="corrected_string",no_parity="no_parity",no_errors="no_errors",shannon_limit="shannon_limit",no_itterations="no_itterations", background_color="", text_color="")
-    elif "tesco" in request.form:
-        dark_mode = True
-        return render_template('index.html', background_color="rgb(80, 80, 80)", text_color="white") # Dark Mode
+        return render_template('form return.html',alice_bits=alice_bits,bob_bits=bob_bits,corrected_string="corrected_string",no_parity="no_parity",no_errors="no_errors",shannon_limit="shannon_limit",no_itterations="no_itterations")
     else:
-        dark_mode = False
-        return render_template('index.html', background_color="", text_color="") # Light Mode
+        return ("<h1>ERROR</h1>")
